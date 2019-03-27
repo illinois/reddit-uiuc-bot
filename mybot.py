3b732c655f10726e366bef81123f2611e36363cf
rosenowak <nowakarose@gmail.com>
2019-03-27 14:10:45 -0500

    just comments on posts not comments

--- mybot.py
+++ mybot.py
@@ -1,4 +1,7 @@
 import praw
+import pdb
+import re
+import os
 
 bot = praw.Reddit(user_agent='classes v0.1',
                   client_id='XoDRN8nBSmglSg',
@@ -7,14 +10,40 @@ bot = praw.Reddit(user_agent='classes v0.1',
                   password='lovecs225')
 
 
-subreddit = bot.subreddit('uiuc')
+subreddit = bot.subreddit('fourpanelcringe')
 comments = subreddit.stream.comments()
 
-for comment in comments:
-    text = comment.body # Fetch body
-    author = comment.author # Fetch author
-    if 'fialed' in text.lower():
-        # Generate a message
-        #message = "Why no love for Titan, u/{0} ?".format(author)
-        message = "Oof"
-        comment.reply(message) # Send message
+
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+
+# If we have run the code before, load the list of posts we have replied to
+else:
+    # Read the file into a list and remove any empty values
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
+
+# Get the top 5 values from our subreddit
+for submission in subreddit.new(limit=5):
+    if submission.id not in posts_replied_to:
+        if re.search("Sublease", submission.title, re.IGNORECASE):
+            submission.reply("No")
+            print("Bot replying to : ", submission.title)
+            posts_replied_to.append(submission.id)
+
+# Write our updated list back to the file
+with open("posts_replied_to.txt", "w") as f:
+    for post_id in posts_replied_to:
+        f.write(post_id + "\n")
+
+#for comment in comments:
+#    text = comment.body # Fetch body
+#    author = comment.author # Fetch author
+#    if 'fialed' in text.lower():
+#        # Generate a message
+#        #message = "Why no love for Titan, u/{0} ?".format(author)
+#        message = "Oof"
+#        comment.reply(message) # Send message
