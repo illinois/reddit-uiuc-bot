5b01e224ed575024a9f6bde7e10b4a8b214d45b8
rosenowak <nowakarose@gmail.com>
2019-11-12 23:32:55 -0600

    yay

--- process_reddit_post.py
+++ process_reddit_post.py
@@ -17,6 +17,11 @@ df_gpa = df_gpa[ df_gpa["Term"] >= "2017-fa" ]
 df_gpa["Number"] = df_gpa["Number"].astype(str)
 df_gpa["Course"] = df_gpa["Subject"] + " " + df_gpa["Number"]
 
+df_gened = pd.read_csv('gen-ed.csv')
+
+df_fa19 = pd.read_csv('uiuc-course-catalog-fa19.csv')
+df_fa19["Number"] = df_fa19["Number"].astype(str)
+df_fa19["Course"] = df_fa19["Subject"] + " " + df_fa19["Number"]
 
 def get_recent_average_gpa(course):
   df = df_gpa[ df_gpa["Course"] == course ].groupby("Course").agg("sum").reset_index()
@@ -32,6 +37,37 @@ def get_recent_average_gpa(course):
   return df["Average GPA"].values[0]
 
 
+def get_all_geneds(course):
+  df = df_gened[ df_gened["Course"] == course ]
+  if len(df) == 0: return None
+
+  gen_eds_offered = []
+  if not df["ACP"].empty:
+      gen_eds_offered.append("Advance Composition")
+  if not df["CS"].empty:
+      gen_eds_offered.append("Cultural Studies")
+  if not df["HUM"].empty:
+      gen_eds_offered.append("Humanities")
+  if not df["NAT"].empty:
+      gen_eds_offered.append("Natural Sciences")
+  if not df["QR"].empty:
+      gen_eds_offered.append("Quantitative Reasoning")
+  if not df["SBS"].empty:
+      gen_eds_offered.append("Social & Behavior")
+
+  if gen_eds_offered == None:
+      return None
+
+  gen_ed_formatted = ""
+  if len(gen_eds_offered) > 1:
+      for i in range(len(gen_eds_offered) - 1):
+          gen_ed_formatted += gen_eds_offered[i]
+          gen_ed_formatted += ", "
+  gen_ed_formatted += gen_eds_offered[len(gen_eds_offered)-1]
+
+  return gen_ed_formatted
+
+
 def get_course_from_crn(crn):
     crn = int(crn)
     df_crnMatch = df_courseSchedule[ df_courseSchedule["CRN"] == crn ]
@@ -41,8 +77,8 @@ def get_course_from_crn(crn):
 
     subject = df_crnMatch['Subject'].values[0]
     number = df_crnMatch['Number'].values[0]
-    course = f"{subject} {number}" 
-    
+    course = f"{subject} {number}"
+
     logging.debug(f"CRN Found: {crn} -> {course}")
     return course
 
@@ -51,14 +87,22 @@ def format_reply_for_course(course):
   subject, number = course.split(" ")
 
   d = df_courseSchedule[ df_courseSchedule["Course"] == course ]
-  if len(d) == 0:
+  actual_class = df_fa19[ df_fa19["Course"] == course ]
+  if len(d) == 0 and len(actual_class) == 0:
     logging.debug(f"No Course Found: {course}")
     return None
 
-  courseName = d["Name"].values[0]
-  courseDescription = d["Description"].values[0].replace("(", "\(").replace(")", "\)")
-  creditHours = d["Credit Hours"].values[0]
-  courseScheduleURL = course_schedule_url_template.replace(":subject", subject).replace(":number", number)
+  if len(d) == 0:
+      courseName = actual_class["Name"].values[0]
+      creditHours = actual_class["Credit Hours"].values[0]
+  else:
+      courseName = d["Name"].values[0]
+      creditHours = d["Credit Hours"].values[0]
+      courseScheduleURL = course_schedule_url_template.replace(":subject", subject).replace(":number", number)
+
+  gen_eds = get_all_geneds(course)
+  if gen_eds == None:
+      gen_eds = "*No gen eds are fullfilled*"
   gpaVizURL = gpa_visualization_url_template.replace(":subject", subject).replace(":number", number)
   avgGPA = get_recent_average_gpa(course)
   if avgGPA == None:
@@ -67,9 +111,18 @@ def format_reply_for_course(course):
     avgGPA = round(avgGPA, 2)
 
   logging.debug(f"Course Found: {course}")
-  return f"**\[{course}\]**: **{courseName}** -- {creditHours}\n" + \
-    f"- ✅ Offered in {courseScheduleTerm} -- [Course Schedule for {course}]({courseScheduleURL})\n" + \
-    f"- Recent Average GPA: **{avgGPA}** -- [GPA Visualization for {course}]({gpaVizURL})\n"
+  if len(d) > 0:
+      return f"**\[{course}\]**: **{courseName}** -- {creditHours}\n" + \
+        f"- ✅ Offered in {courseScheduleTerm} -- [Course Schedule for {course}]({courseScheduleURL})\n" + \
+        f"- Recent Average GPA: **{avgGPA}** -- [GPA Visualization for {course}]({gpaVizURL})\n"+ \
+        f"- Gen Eds Fullfilled: {gen_eds}\n"
+  else:
+      return f"**\[{course}\]**: **{courseName}** -- {creditHours}\n" + \
+        f"- ❌ Not offered in {courseScheduleTerm}\n" + \
+        f"- Recent Average GPA: **{avgGPA}** -- [GPA Visualization for {course}]({gpaVizURL})\n"+ \
+        f"- Gen Eds Fullfilled: {gen_eds}\n"
+
+
 
 
 def get_reply_from_submission(s, id=-1):
@@ -111,7 +164,7 @@ def get_reply_from_submission(s, id=-1):
   # Prevent the output from being HUGE
   if len(courseInfos) > 10:
     courseInfos = courseInfos[0:9]
-  
+
   # Join all the courseInfo strings together with an <hr> between them:
   if len(courseInfos) == 0:
     return None
--- reddit-gpa-bot.py
+++ reddit-gpa-bot.py
@@ -7,19 +7,17 @@ import csv
 import time
 import pandas as pd
 import numpy as np
-import logging
 from process_reddit_post import get_reply_from_submission
 
 df = pd.read_csv('gpa-dataset.csv')
 df = df[ df["YearTerm"] >= '2017-fa' ]
 
-bot_username = "classbotuiuc"
 
 # Reddit Bot Init
 bot = praw.Reddit(user_agent='classes v0.1',
     client_id='XoDRN8nBSmglSg',
     client_secret='RBW92Bu-hmwaMnXIs74VLB2n-V4',
-    username=bot_username,
+    username='classbotuiuc',
     password='lovecs225')
 subreddit = bot.subreddit('testingground4bots')
 comments = subreddit.stream.comments()
@@ -34,56 +32,37 @@ def main():
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
 
-    # Bot Logic/Processing
-    # for submission in subreddit.new(limit=20):
-    #   if submission.id not in posts_replied_to:
-    #     # Use both the title of the post and body:
-    #     #for comments in submission.comments:
-    #     s = submission.title + " " + submission.selftext
-    #     reply = get_reply_from_submission(s, submission.id)
+    #Bot Logic/Processing
+    for submission in subreddit.new(limit=20):
+      if submission.id not in posts_replied_to:
+        # Use both the title of the post and body:
+        #for comments in submission.comments:
+        s = submission.title + " " + submission.selftext
+        reply = get_reply_from_submission(s, submission.id)
 
+        # Reply and record reply:
+        if reply:
+          submission.reply(reply)
+          print(f"Bot replying to: {submission.title}")
+
+        posts_replied_to.append(submission.id)
+
+    # for comment in subreddit.stream.comments():
+    #   if comment.id not in posts_replied_to:
+    #     s = comment.body
+    #     reply = get_reply_from_submission(s, comment.id)
+    #
     #     # Reply and record reply:
     #     if reply:
-    #       submission.reply(reply)
-    #       print(f"Bot replying to: {submission.title}")
-
-    #     posts_replied_to.append(submission.id)
-
-    for comment in subreddit.stream.comments():
-      id = comment.id
-
-      # Never reply to yourself:
-      if comment.author == bot_username:
-        logging.debug(f"[{id}] Skipping -- should not reply to self.")
-        continue
-
-      # Never reply to the same post again (in case the bot restarts):
-      if comment.id in posts_replied_to:
-        logging.debug(f"[{id}] Skipping -- reply already made.")
-        continue
-
-      # Parse the comment for a reply:
-      s = comment.body.replace('\\[', '[').replace('\\]', ']')
-      print(s)
-      reply = get_reply_from_submission(s, comment.id)
-
-      # Reply and record reply:
-      if reply:
-        comment.reply(reply)
-        print(f"Bot replying to: {comment.author}")
-        posts_replied_to.append(comment.id)
-      else:
-        logging.debug(f"[{id}] No reply -- no matching string found.")
-
-
+    #       comment.reply(reply)
+    #       print(f"Bot replying to: {comment.author}")
+    #
+    #     posts_replied_to.append(comment.id)
+    #
     with open("posts_replied_to.txt", "w") as f:
       for post_id in posts_replied_to:
         f.write(post_id + "\n")
 
-    print("im out")
-
-
-main()
-# while 1:
-#   main()
-#   time.sleep(5) #Wait 5 Seconds
+while 1:
+  main()
+  time.sleep(5) #Wait 5 Seconds
